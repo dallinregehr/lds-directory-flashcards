24f014048c4a2212dd1bc5b06139e2e2a3737327
Dallin Regehr <dallinregehr@gmail.com>
2026-05-17 23:03:14 -0600

    Remove obsolete load-chrome.sh and load-firefox.sh

--- load-chrome.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp manifest.chrome.json manifest.json
-echo "Ready to load in Chrome: open chrome://extensions, enable Developer mode, and Load unpacked."
--- load-firefox.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp manifest.firefox.json manifest.json
-echo "Ready to load in Firefox/Zen: open about:debugging, click This Firefox, and Load Temporary Add-on."
